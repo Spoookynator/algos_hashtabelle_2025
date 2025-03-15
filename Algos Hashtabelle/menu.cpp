0a2783c481557e4096df363ae35c9c228948c2fb
Spoookynator <99873987+Spoookynator@users.noreply.github.com>
2025-03-15 16:23:17 +0100

    added menu functionality with function pointers, not fully tested, should work fine tho

---
+++ Algos Hashtabelle/menu.cpp
@@ -0,0 +1,100 @@
+#include "menu.h"
+#include <iostream>
+#include <string>
+
+Menu::Menu() {}
+
+int Menu::getInput()
+{
+	std::string input;
+	std::getline(std::cin, input); // get whole line, not just one string
+	
+	std::string split[2];
+	int pos = input.find(" ");
+	
+	split[0] = input.substr(0, pos); // somehow this works even if no space is given, it should be 0, but maybe thats a substr quirk
+	split[1] = "";
+
+
+	// adds second string only if a space was found
+	if (pos != std::string::npos)
+	{
+		split[1] = input.substr(pos + 1);
+	}
+
+	// help functionality, this is not an extra input
+	if (split[0] == "HELP" || split[0] == "help")
+	{
+		help();
+		return 1;
+	}
+
+	// same as help, returns -1 to tell the menu to quit
+	if (split[0] == "QUIT" || split[0] == "quit")
+	{
+		std::cout << "quitting...";
+		return -1;
+	}
+
+	return executeInput(split);
+}
+
+void Menu::addInput(std::vector<std::string> keywords, funcPtr func, std::string description)
+{
+	if (this->inputLen < 10) // currently max inputs is 10, but that could be changed to a constant
+	{
+		this->inputs[inputLen++] = Input(keywords, func, description);
+	}
+	else {
+		std::cout << "can't add more inputs\n!";
+	}
+}
+
+void Menu::startLoop()
+{
+	int output = 1;
+	
+	while (output != -1)
+	{
+		do
+		{
+			if (output == 0)
+			{
+				std::cout << "Wrong input, type HELP to get help!\n";
+			}
+			output = this->getInput();
+		} while (output == 0);
+	}
+}
+
+bool Menu::executeInput(std::string input[2])
+{
+	for (int i = 0; i < inputLen; i++)
+	{
+		if (std::find(inputs[i].keywords.begin(), inputs[i].keywords.end(), input[0]) != inputs[i].keywords.end()) {
+			return inputs[i].function(input[1]);
+		}
+	}
+	
+	return false;
+}
+
+
+void Menu::help()
+{
+	for (int i = 0; i < inputLen; i++)
+	{
+		std::cout << this->inputs[i].keywords.at(0) << ": " << this->inputs[i].description << "\n";
+	}
+	
+	std::cout << "Quit: exits the program\n";
+}
+
+Input::Input(std::vector<std::string> keywords, funcPtr func, std::string description) : keywords(keywords), function(func), description(description) {}
+
+Input::Input()
+{
+	this->function = nullptr;
+	this->keywords = {};
+	this->description = "";
+}
---
+++ Algos Hashtabelle/menu.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <vector>
+#include <string>
+
+typedef bool(*funcPtr)(std::string);
+
+struct Input
+{
+	/// <summary>
+	/// New input element
+	/// </summary>
+	/// <param name="Keywords">(adds keywords for command recognition)</param>
+	/// <param name="Bound Function"></param>
+	/// <param name="Description">(adds description for help command)</param>
+	Input(std::vector<std::string>, funcPtr, std::string = "");
+	Input();
+
+	std::vector<std::string> keywords;
+	funcPtr function;
+	std::string description;
+};
+
+class Menu
+{
+public:
+	Menu();
+
+	/// <summary>
+	/// Bind inputs to the menus
+	/// </summary>
+	/// <param name="Keywords"></param>
+	/// <param name="Bound Function"></param>
+	/// <param name="Description"></param>
+	void addInput(std::vector<std::string>, funcPtr, std::string);
+
+	// starts the main loop, all operations will happen here
+	void startLoop();
+
+private:
+	// gets input from user, returns 0 for false, 1 for true and -1 for exit program
+	int getInput();
+
+	// prints description of all inputs (+ the default quit option)
+	void help();
+
+	/// <summary>
+	/// Executes input with parameters given, needs Input struct to work
+	/// </summary>
+	/// <param name="Input">(Input struct with keywords and func poiner)</param>
+	/// <returns></returns>
+	bool executeInput(std::string[2]);
+
+
+	short int inputLen = 0;
+	Input inputs[10] = {};
+};
