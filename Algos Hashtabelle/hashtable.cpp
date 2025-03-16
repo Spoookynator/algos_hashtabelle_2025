3e39de569d282156b8e60f2839c230e11239e5a1
Spoookynator <99873987+Spoookynator@users.noreply.github.com>
2025-03-16 03:36:08 +0100

    changed hashtable to use the heap and added constructors etc; should be pretty safe, not 100% sure about it tho

--- Algos Hashtabelle/hashtable.cpp
+++ Algos Hashtabelle/hashtable.cpp
@@ -1,15 +1,15 @@
 #include "hashtable.h"
 
-hashtable::hashtable() {
-	// empty initialization
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		table[i].stockId.name = "";
-		table[i].stockId.abbreviation = "";
-		std::cout << "table[" << i << "].stockId.name: " << table[i].stockId.name << std::endl;
+Hashtable::Hashtable() {
+
+	// init table
+	for (int i = 0; i < 30; i++)
+	{
+		table[i] = new Entry();
 	}
 }
 
-int hashtable::calculateHash(string abbreviation) {
+int Hashtable::calculateHash(std::string abbreviation) {
 	// hash function
 	int hashIndex = 0;
 	for (int i = 0; i < abbreviation.length(); i++) {
@@ -19,3 +19,76 @@ int hashtable::calculateHash(string abbreviation) {
 	return hashIndex;
 }
 
+StockEntry::StockEntry(Id id, Data* data[30]) : Entry(false, true), stockId(id)
+{
+	for (int i = 0; i < 30; i++)
+	{
+		if (data[i]) {
+			this->stockData[i] = new Data(*data[i]);
+		}
+	}
+	this->occupied = false;
+}
+
+StockEntry::~StockEntry()
+{
+	for (int i = 0; i < 30; i++)
+	{
+		delete this->stockData[i];
+	}
+}
+
+const Data* const* StockEntry::getData() const
+{
+	return this->stockData;
+}
+
+const Id* const StockEntry::getId() const
+{
+	return &stockId;
+}
+
+Entry::Entry(bool empty, bool occupied)
+{
+	this->empty = empty;
+	this->occupied = occupied;
+}
+
+const Data* const* Entry::getData() const
+{
+	return nullptr;
+}
+
+const Id* const Entry::getId() const
+{
+	return nullptr;
+}
+
+bool Entry::isOccupied() const
+{
+	return occupied;
+}
+
+bool Entry::isEmpty() const
+{
+	return empty;
+}
+
+Data::Data(std::string date, double close, int volume, double open, double high, double low) : date(date), close(close), volume(volume), open(open), high(high), low(low)
+{
+
+}
+
+Data::Data(const Data& other)
+{
+	this->close = other.close;
+	this->date = other.date;
+	this->high = other.high;
+	this->low = other.low;
+	this->open = other.open;
+	this->volume = other.volume;
+}
+
+Id::Id(std::string name, std::string abbreviation) : name(name), abbreviation(abbreviation)
+{
+}
--- Algos Hashtabelle/hashtable.h
+++ Algos Hashtabelle/hashtable.h
@@ -4,32 +4,58 @@
 #include <string>
 #define TABLE_SIZE 2003 //next largest prime number after 2000, reflecting the intended capacity of 1000 total entries and 50% load factor
 
-using std::string;
 
-class hashtable {
+struct Data{
+	Data(std::string, double, int, double, double, double);
+	Data(const Data&); // copy constructor
+
+	std::string date; // format: mm/dd/yyyy
+	double close;
+	int volume;
+	double open;
+	double high;
+	double low;
+};
+
+struct Id {
+	Id(std::string, std::string);
+	std::string name;
+	std::string abbreviation;
+};
+
+struct Entry
+{
+	Entry(bool = true, bool = false);
+	virtual ~Entry() = default;
+	bool occupied; // tracks if the stock was occupied before, for safe deletion
+	bool empty;
+
+	// double const so the pointer cant be changed and the value also cant be changed
+	// this returns nullptr when there is no data stored
+	virtual const Data* const* getData() const;
+	virtual const Id* const getId() const;
+
+	bool isOccupied() const;
+	bool isEmpty() const;
+};
+
+struct StockEntry : public Entry {
+
+	StockEntry(Id, Data*[30]);
+	~StockEntry();
+	Id stockId;
+	Data* stockData[30] = {};
+
+	const Data* const * getData() const override;
+	const Id* const getId() const override;
+};
+
+class Hashtable {
 public:
-	hashtable();
-	int calculateHash(string abbreviation);
+	Hashtable();
+
+	int calculateHash(std::string abbreviation);
 
 private:
-	typedef struct {
-		string name;
-		string abbreviation;
-	} id;
-
-	typedef struct {
-		string date; // format: mm/dd/yyyy
-		double close;
-		int volume;
-		double open;
-		double high;
-		double low;
-	} data;
-
-	typedef struct {
-		id stockId;
-		data stockData[30];
-	} stockEntry;
-
-	stockEntry table[TABLE_SIZE];
+	Entry* table[TABLE_SIZE];
 };
